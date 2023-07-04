4d1843aa3d84d5d785459a1c5b5e190f41cde58a
HDTian <haodongt1123@gmail.com>
2023-07-04 18:31:13 +0100

    new PC: same

--- R/Real.R
+++ R/Real.R
@@ -1,10 +1,32 @@
 
 
 
-
-
-
 #Real application code
+dim(Dat)#Dat: the real data with the same form as the simulated data (i.e ID+Z+X+Y+M) no HTE labels
+
+apply(Dat, 2, is.numeric)
+apply(Dat, 2, is.character)
+
+NNNN<-dim(Dat)[1]
+NNN<-round(NNNN*2/3)
+NNNN;NNN#For male: 142186(NNNN) 94791(NNN)
+
+Dat<-as.data.frame(Dat)
+names(Dat)[1]<-'I'
+names(Dat)[3]<-'X'
+names(Dat)[4]<-'Y'
+colnames(Dat)
+
+#同一个Dat下，odat和vdat永远不会改变 (odat vdat在全局环境)
+vdat<-Dat[(NNN+1):NNNN, ]  #validation dat
+odat<-Dat[1:NNN,]   #original (training) dat
+
+
+#RFQT fitting
+ALLRES_real<-RFQTfit(odat,vdat,Nb=200,method='DR')
+saveRDS(ALLRES,file='D:\\files\\R new\\Precison_Medicine\\ALLRES_rdata\\test.RData')
+ALLRES_<-readRDS('D:\\files\\R new\\Precison_Medicine\\ALLRES_rdata\\test.RData')
+
 
 ###result1: (Figure 4)
 #histogram plot of the testing set predicts using (DR Nb=200)
