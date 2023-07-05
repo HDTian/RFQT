438d1abf7587f995f5f5f810a29ab2e32eb94d4d
HDTian <haodongt1123@gmail.com>
2023-07-05 23:53:55 +0100

    updates

--- R/Real.R
+++ R/Real.R
@@ -27,7 +27,7 @@ odat<-Dat[1:NNN,]   #original (training) dat
 
 
 #RFQT fitting
-ALLRES_real<-RFQTfit(odat,vdat,Nb=200,method='DR')\
+ALLRES_real<-RFQTfit(odat,vdat,Nb=200,method='DR')
 
 #saveRDS(ALLRES_real,file='D:\\files\\R new\\Precison_Medicine\\real_data\\ALLRES_real.RData')
 #ALLRES_real<-readRDS('D:\\files\\R new\\Precison_Medicine\\real_data\\ALLRES_real.RData')
@@ -36,27 +36,55 @@ ALLRES_real<-RFQTfit(odat,vdat,Nb=200,method='DR')\
 ###result1: (Figure 4)
 #histogram plot of the testing set predicts using (DR Nb=200)
 summary(  ALLRES_real$Predicts_test[,200]  )
-#     Min.   1st Qu.    Median      Mean   3rd Qu.      Max. 
+#     Min.   1st Qu.    Median      Mean   3rd Qu.      Max.   (honest)
 #-0.717172 -0.028483 -0.019300 -0.020037 -0.009268  0.531695 
+#     Min.   1st Qu.    Median      Mean   3rd Qu.      Max.   (nonhonest)
+#-0.065441 -0.027532 -0.017536 -0.014073 -0.003499  0.061977
 
+hist( ALLRES_real$Predicts_test[,100]  , n=200, main='', xlab='Predicted effect')
+#hist( ALLRES_real$Predicts_test[,200]  , n=500, xlim=c(-0.1,0.05) , main='', xlab='Predicted effect')
 
-hist( ALLRES_real$Predicts_test[,200]  , n=500, xlim=c(-0.1,0.05) , main='', xlab='Predicted effect')
+ggdata<-data.frame(Predicted_effect= ALLRES_real$Predicts_test[,100] )
+p<-ggplot(ggdata) +
+  geom_bar(aes(Predicted_effect)) +
+  scale_x_binned(n.breaks=30)+
+  geom_abline(intercept = 0, slope = 1000000000000000000, colour='grey', linewidth=1.0 ,linetype = 2)+
+  xlab('Predicted effect')+ylab('Count')+
+  theme_bw()+theme(panel.grid.major=element_blank(),panel.grid.minor=element_blank())
+p
+ggsave(paste0('Fig4.eps' ),   #.eps  #real以后都特指R3： BMI on fev1 for the female
+       plot =p ,  #非指定 
+       path=paste0('C:\\Users\\Haodong\\Desktop\\Precision_Medicine_new\\newplots'), 
+       height = 5, width = 15, units = "in",limitsize=TRUE)
 
 
 ###results2: (Figure S2)
 #Variable importance plot
+VIindex<-order(ALLRES_real$VI2)
 
+ggdata_VI<-data.frame(candidate=factor(names(Dat)[VIindex+4],levels =(names(Dat)[VIindex+4])[JJ:1] ),  #逆向排序
+                      VI=( ALLRES_real$VI2 )[VIindex]   )  
+p<-ggplot(ggdata_VI,aes(x=candidate,y=VI))+geom_col( )+
+  xlab('Candidate')+ylab('Variable Importance')+#labs(title=c('Doubly-ranked method', 'Residual method' , 'Naive method'   )[RESindex] )+
+  theme_bw()+theme(panel.grid.major=element_blank(),panel.grid.minor=element_blank())+
+  coord_flip()
+p
+ggsave(paste0('Fig_S2.eps' ),   #.eps  #real以后都特指R3： BMI on fev1 for the female
+       plot =p ,  #非指定 
+       path=paste0('C:\\Users\\Haodong\\Desktop\\Precision_Medicine_new\\newplots'), 
+       height = 6, width = 4, units = "in",limitsize=TRUE)
 
 
 ###result3: (Figure 5)
-#important variable -> marginal stratum analysis using pre-detetmined quantile points
+#important variable -> marginal stratum analysis using pre-determined (tenth) quantile points
 #one can use DRMR package
-#Q statistic use average; point and CIS use Rubin's Rule
+#Q statistic use average; point and CIS use Rubin's Rule (RR)
 
 
 
 ###result4: (new Figure)
 #according to vdat predicted HTE (i.e. v_predict), refit HTE ~ M to visualize the results or for future guidance
+
 
 
 
